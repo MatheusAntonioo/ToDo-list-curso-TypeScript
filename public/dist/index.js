73a1459f82325b0be47438d850f1c04f8c47e631
Matheus Antonio <dev.matheus.antonio@outlook.com>
2023-04-18 00:06:06 -0300

    dia 17/04/23 att2

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,36 +1,138 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 (function () {
-    var todo = {
-        description: 'todo',
-        done: false,
+    var NotificationPlatform;
+    (function (NotificationPlatform) {
+        NotificationPlatform["SMS"] = "SMS";
+        NotificationPlatform["EMAIL"] = "EMAIL";
+        NotificationPlatform["PUSH_NOTIFICATION"] = "PUSH_NOTIFICATION";
+    })(NotificationPlatform || (NotificationPlatform = {}));
+    var ViewMode;
+    (function (ViewMode) {
+        ViewMode["TODO"] = "TODO";
+        ViewMode["REMINDER"] = "REMINDER";
+    })(ViewMode || (ViewMode = {}));
+    var UUID = function () {
+        return Math.random().toString(32).substr(2, 9);
     };
-    var reminder = {
-        description: 'reminder',
-        date: '17.04.2021',
+    var DateUtils = {
+        tomorrow: function () {
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            return tomorrow;
+        },
+        today: function () {
+            return new Date();
+        },
+        formatDate: function (date) {
+            return "".concat(date.getDate(), ".").concat(date.getMonth() + 1, ".").concat(date.getFullYear());
+        }
     };
+    var Reminder = /** @class */ (function () {
+        function Reminder(description, date, notifications) {
+            this.id = UUID();
+            this.dateCreated = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
+            this.description = '';
+            this.date = DateUtils.tomorrow();
+            this.notifications = [NotificationPlatform.EMAIL];
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+        Reminder.prototype.render = function () {
+            return "\n      ---> Reminder <---\n      description: ".concat(this.description, "\n      date: ").concat(DateUtils.formatDate(this.date), "\n      platform: ").concat(this.notifications.join(','), "\n      ");
+        };
+        return Reminder;
+    }());
+    var Todo = /** @class */ (function () {
+        function Todo(description) {
+            this.id = UUID();
+            this.dateCreated = DateUtils.today();
+            this.dateUpdated = DateUtils.today();
+            this.description = '';
+            this.done = false;
+            this.description = description;
+        }
+        Todo.prototype.render = function () {
+            return "\n      ---> TODO <---\n      description: ".concat(this.description, "\n      done: ").concat(this.done, "\n      ");
+        };
+        return Todo;
+    }());
+    var todo = new Todo('Todo criado com a classe');
+    var reminder = new Reminder('Reminder criado com a classe', new Date(), [NotificationPlatform.EMAIL,]);
     var taskView = {
-        render: function (tasks) {
+        getTodo: function (form) {
+            var todoDescription = form.todoDescription.value;
+            form.reset();
+            return new Todo(todoDescription);
+        },
+        getReminder: function (form) {
+            var reminderNotifications = [
+                form.notifications.value,
+            ];
+            var reminderDate = new Date(form.reminderDate.value);
+            var reminderDescription = form.reminderDescription.value;
+            form.reset();
+            return new Reminder(reminderDescription, reminderDate, reminderNotifications);
+        },
+        render: function (tasks, mode) {
             var tasksList = document.getElementById('tasksList');
             while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
                 tasksList.removeChild(tasksList.firstChild);
             }
             tasks.forEach(function (task) {
                 var li = document.createElement('LI');
-                var textNode = document.createTextNode(JSON.stringify(task));
+                var textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
             });
+            var todoSet = document.getElementById('todoSet');
+            var reminderSet = document.getElementById('reminderSet');
+            if (mode === ViewMode.TODO) {
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute('style', 'display: block');
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.removeAttribute('disabled');
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute('style', 'display: none');
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute('disabled', 'true');
+            }
+            else {
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute('style', 'display: block');
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.removeAttribute('disabled');
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute('style', 'display: none');
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute('disabled', 'true');
+            }
         },
     };
     var TaskController = function (view) {
-        var _a;
-        var tasks = [todo, reminder];
+        var _a, _b;
+        var tasks = [];
+        var mode = ViewMode.TODO;
         var handleEvent = function (event) {
             event.preventDefault();
-            view.render(tasks);
+            var form = event.target;
+            switch (mode) {
+                case ViewMode.TODO:
+                    tasks.push(view.getTodo(form));
+                    break;
+                case ViewMode.REMINDER:
+                    tasks.push(view.getReminder(form));
+                    break;
+            }
+            view.render(tasks, mode);
         };
-        (_a = document.getElementById('taskForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', handleEvent);
+        var handleToggleMode = function () {
+            switch (mode) {
+                case ViewMode.TODO:
+                    mode = ViewMode.REMINDER;
+                    break;
+                case ViewMode.REMINDER:
+                    mode = ViewMode.TODO;
+                    break;
+            }
+            view.render(tasks, mode);
+        };
+        (_a = document.getElementById('toggleMode')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', handleToggleMode);
+        (_b = document.getElementById('taskForm')) === null || _b === void 0 ? void 0 : _b.addEventListener('submit', handleEvent);
     };
     TaskController(taskView);
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,117 @@
 import { Type } from "typescript";
 
+
+
 (() => {
-  const todo = {
-    description: 'todo',
-    done: false,
+  enum NotificationPlatform{
+    SMS = 'SMS',
+    EMAIL = 'EMAIL',
+    PUSH_NOTIFICATION = 'PUSH_NOTIFICATION',
+  }
+
+  enum ViewMode {
+    TODO = 'TODO',
+    REMINDER = 'REMINDER'
+  }
+
+  const UUID = (): string => {
+    return Math.random().toString(32).substr(2, 9);
   };
 
-  const reminder = {
-    description: 'reminder',
-    date: '17.04.2021',
+  const DateUtils = {
+    tomorrow(): Date {
+      const tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      return tomorrow;
+    },
+    today(): Date {
+      return new Date();
+    },
+    formatDate(date: Date): string {
+      return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+    }
   };
+
+  interface Task {
+    id: string;
+    dateCreated: Date;
+    dateUpdated: Date;
+    description: string;
+    render(): string;
+  }
+
+  class Reminder implements Task {
+    id: string = UUID();
+    dateCreated: Date = DateUtils.today();
+    dateUpdated: Date = DateUtils.today();
+    description: string = '';
+
+    date: Date = DateUtils.tomorrow();
+    notifications: Array<NotificationPlatform> = [NotificationPlatform.EMAIL];
+
+    constructor(description: string, date: Date, notifications: Array<NotificationPlatform>) {
+      this.description = description;
+      this.date = date;
+      this.notifications = notifications;
+    }
+
+    render(): string {
+      return `
+      ---> Reminder <---
+      description: ${this.description}
+      date: ${DateUtils.formatDate(this.date)}
+      platform: ${this.notifications.join(',')}
+      `;
+    }
+    
+  }
+
+  class Todo implements Task {
+    id: string = UUID();
+    dateCreated: Date = DateUtils.today();
+    dateUpdated: Date = DateUtils.today();
+    description: string = '';
+
+    done: boolean = false;
+
+    constructor(description: string) {
+      this.description = description;
+    }
+
+    render(): string {
+      return `
+      ---> TODO <---
+      description: ${this.description}
+      done: ${this.done}
+      `;
+    }
+    
+  }
+  
+  const todo = new Todo('Todo criado com a classe');
+
+  const reminder = new Reminder('Reminder criado com a classe', new Date(), [NotificationPlatform.EMAIL,]);
 
   const taskView = {
-    render(tasks: Array<Object>) {
+    getTodo(form: HTMLFormElement): Todo {
+      const todoDescription = form.todoDescription.value;
+      form.reset();
+      return new Todo(todoDescription);
+    },
+    getReminder(form: HTMLFormElement): Reminder {
+      const reminderNotifications = [
+        form.notifications.value as NotificationPlatform,
+      ];
+      const reminderDate = new Date(form.reminderDate.value);
+      const reminderDescription = form.reminderDescription.value;
+      form.reset();
+      return new Reminder(
+        reminderDescription,
+        reminderDate,
+        reminderNotifications
+      );
+    },
+    render(tasks: Array<Task>, mode: ViewMode) {
       const tasksList = document.getElementById('tasksList');
       while(tasksList?.firstChild) {
         tasksList.removeChild(tasksList.firstChild);
@@ -20,21 +119,58 @@ import { Type } from "typescript";
 
       tasks.forEach((task) => {
         const li = document.createElement('LI');
-        const textNode = document.createTextNode(JSON.stringify(task));
+        const textNode = document.createTextNode(task.render());
         li.appendChild(textNode);
         tasksList?.appendChild(li);
       });
+
+      const todoSet = document.getElementById('todoSet');
+      const reminderSet = document.getElementById('reminderSet');
+
+      if (mode === ViewMode.TODO) {
+        todoSet?.setAttribute('style', 'display: block');
+        todoSet?.removeAttribute('disabled');
+        reminderSet?.setAttribute('style', 'display: none');
+        reminderSet?.setAttribute('disabled', 'true');
+      } else {
+        reminderSet?.setAttribute('style', 'display: block');
+        reminderSet?.removeAttribute('disabled');
+        todoSet?.setAttribute('style', 'display: none');
+        todoSet?.setAttribute('disabled', 'true');
+      }
     },
   };
 
   const TaskController = (view: typeof taskView) => {
-    const tasks: Array<Object> = [todo, reminder];
+    const tasks: Array<Task> = [];
+    let mode: ViewMode = ViewMode.TODO;
 
     const handleEvent = (event: Event) => {
       event.preventDefault();
-      view.render(tasks);
+      const form = event.target as HTMLFormElement;
+      switch (mode as ViewMode){
+        case ViewMode.TODO:
+          tasks.push(view.getTodo(form));
+          break;
+        case ViewMode.REMINDER:
+          tasks.push(view.getReminder(form));
+          break;
+      }
+      view.render(tasks, mode);
     };
 
+    const handleToggleMode = () => {
+      switch (mode as ViewMode) {
+        case ViewMode.TODO:
+          mode = ViewMode.REMINDER
+          break;
+        case ViewMode.REMINDER:
+          mode = ViewMode.TODO
+          break
+      }
+      view.render(tasks, mode);
+    };
+    document.getElementById('toggleMode')?.addEventListener('click', handleToggleMode);
     document.getElementById('taskForm')?.addEventListener('submit', handleEvent);
   };
 
