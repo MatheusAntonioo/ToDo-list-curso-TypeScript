5eb7e645f59b2f584013302d3a53e8222cb10898
Matheus Antonio <dev.matheus.antonio@outlook.com>
2023-04-17 21:08:38 -0300

    dia 17/04/23 att1

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,2 +1,36 @@
 "use strict";
-alert("Bora iniciar esse projeto de TS.");
+Object.defineProperty(exports, "__esModule", { value: true });
+(function () {
+    var todo = {
+        description: 'todo',
+        done: false,
+    };
+    var reminder = {
+        description: 'reminder',
+        date: '17.04.2021',
+    };
+    var taskView = {
+        render: function (tasks) {
+            var tasksList = document.getElementById('tasksList');
+            while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
+                tasksList.removeChild(tasksList.firstChild);
+            }
+            tasks.forEach(function (task) {
+                var li = document.createElement('LI');
+                var textNode = document.createTextNode(JSON.stringify(task));
+                li.appendChild(textNode);
+                tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
+            });
+        },
+    };
+    var TaskController = function (view) {
+        var _a;
+        var tasks = [todo, reminder];
+        var handleEvent = function (event) {
+            event.preventDefault();
+            view.render(tasks);
+        };
+        (_a = document.getElementById('taskForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', handleEvent);
+    };
+    TaskController(taskView);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-alert("Bora iniciar esse projeto de TS.");
+import { Type } from "typescript";
+
+(() => {
+  const todo = {
+    description: 'todo',
+    done: false,
+  };
+
+  const reminder = {
+    description: 'reminder',
+    date: '17.04.2021',
+  };
+
+  const taskView = {
+    render(tasks: Array<Object>) {
+      const tasksList = document.getElementById('tasksList');
+      while(tasksList?.firstChild) {
+        tasksList.removeChild(tasksList.firstChild);
+      }
+
+      tasks.forEach((task) => {
+        const li = document.createElement('LI');
+        const textNode = document.createTextNode(JSON.stringify(task));
+        li.appendChild(textNode);
+        tasksList?.appendChild(li);
+      });
+    },
+  };
+
+  const TaskController = (view: typeof taskView) => {
+    const tasks: Array<Object> = [todo, reminder];
+
+    const handleEvent = (event: Event) => {
+      event.preventDefault();
+      view.render(tasks);
+    };
+
+    document.getElementById('taskForm')?.addEventListener('submit', handleEvent);
+  };
+
+  TaskController(taskView);
+})();
